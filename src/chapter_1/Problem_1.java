967eadf71029356dd5bc0f698b583f94ec553dab
WillMao <maolingwei218@163.com>
2017-09-08 21:57:29 +0800

    create repository

---
+++ src/chapter_1/Problem_1.java
@@ -0,0 +1,11 @@
+package chapter_1;
+
+public class Problem_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("handsome");
+		System.out.print("帅");
+	}
+
+}
