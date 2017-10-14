143c00bde01bd3c26fae2983debf51341f0addad
zjumlw <maolingwei218@163.com>
2017-10-15 00:21:56 +0800

    update txt files

--- src/chapter_14_QuizCard/ReadAFile.java
+++ src/chapter_14_QuizCard/ReadAFile.java
@@ -11,8 +11,8 @@ public class ReadAFile {
 	public static void main(String[] args) {
 		try{
 			File myFile = new File("E:\\Test\\Java\\HeadFirstJava\\src\\chapter_14_QuizCard\\Foo.txt");
-//			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(myFile)));
-			BufferedReader reader = new BufferedReader(new FileReader(myFile));	//查看是否注释问题
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(myFile),"GBK"));
+//			BufferedReader reader = new BufferedReader(new FileReader(myFile));	//查看是否注释问题
 			String line = null;
 			while((line = reader.readLine()) != null){
 				System.out.println(line);
